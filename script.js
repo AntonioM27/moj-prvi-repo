6f811883e47f4671a18d36e86e13f7754338b75f
AntonioM27 <antoniomikolic1998@gmail.com>
2025-05-20 18:51:26 +0200

    chore:ignore

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Pozdrav od drugog developera");
+
+// git add.
+// git commit -m "poruka"
+//git push origin master ili ime brancha
+//git branch ime
+//git checkout ime
+//git merge ime brancha
